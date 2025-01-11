f0f8f86b12b51e1602512ed0f6a011e1769636f9
Asadshaik17 <shaik_mohammad2023@cms.ac.in>
2025-01-11 16:15:43 +0530

    AffiliateData insterted

--- server/index.js
+++ server/index.js
@@ -1,22 +1,30 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import clientRoutes from './routes/client.js';
-import generalRoutes from './routes/general.js';
-import managementRoutes from './routes/management.js';
-import salesRoutes from './routes/sales.js';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 // Data import
 import User from "./models/User.js";
-import Product from './models/Product.js';
-import ProductStat from './models/ProductStat.js';
-import Transaction  from './models/Transaction.js';
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
-import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from "./data/index.js"
+import AffiliateStat from "./models/AffiliateStat.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+  dataAffiliateStat,
+} from "./data/index.js";
 
 // CONFIGURATION //
 dotenv.config();
@@ -38,15 +46,16 @@ app.use("/sales", salesRoutes);
 // MONGOOSE SETUP //
 const PORT = process.env.PORT || 9000;
 mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => {
-        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-        // Only Once
-        // User.insertMany(dataUser);
-        // Product.insertMany(dataProduct);
-        // ProductStat.insertMany(dataProductStat);
-        // Transaction.insertMany(dataTransaction);
-        // OverallStat.insertMany(dataOverallStat);
-    })
-    .catch((error) => console.log(`${error} did not connect!`));
+    // Only Once
+    // User.insertMany(dataUser);
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
+    // OverallStat.insertMany(dataOverallStat);
+    // AffiliateStat.insertMany(dataAffiliateStat);
+  })
+  .catch((error) => console.log(`${error} did not connect!`));
